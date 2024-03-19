5c7aee63737b9673c0b294bf86ce35b066860ed9
Robis-RCS <robis.rcs@gmail.com>
2024-03-19 17:45:59 +0530

    Side bar max completed
    The side bar has been completed with some features that need to get done. this is the last push on 19-March-2024.

--- RCS/src/App.jsx
+++ RCS/src/App.jsx
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Sidebar from "./Components/Sidebar/Sidebar"
-import Loginpage from './Components/login/Loginpage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +7,6 @@ function App() {
   return (
     <div  className='w-full h-full p-0 m-0'>
       <Sidebar/> 
-      <Loginpage/>
     </div>
   )
 }
--- RCS/src/Components/Sidebar/Sidebar.jsx
+++ RCS/src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import MothersonS from "../../assets/Logo/Motherson-s.png"
+import DropDown from './DropDown'
 
 const Sidebar = () => {
   return (
     <div className='w-[25%] h-full '>
-        <div className='w-[calc(100%-20px)] h-full bg-red-700'>
-            <div className='w-10 h-10'>
-                <img src={MothersonS} alt="" />
+        <div className='w-[calc(100%-20px)] h-full bg-[#494949]'>
+            <div className='p-6 bg-black rounded-tl-[46px] rounded-br-[46px] flex items-center justify-center drop-shadow-xl'>
+                <img src={MothersonS} alt="" className='w-[55px]' />
+            </div>
+            <div className='w-full py-5 flex flex-col justify-center items-center'>
+                <DropDown title={"Menu"} />
+                <DropDown title={"Menu"} />
+                <DropDown title={"Menu"} />
             </div>
         </div>
     </div>
