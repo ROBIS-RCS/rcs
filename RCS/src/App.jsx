5f5a93602bed0ba3ca6cc0cfda6062d9bdfb61a6
ROBIS <robis.rosbot@gmail.com>
2024-03-19 15:34:25 +0530

    changes in login

--- RCS/src/App.jsx
+++ RCS/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from "./Components/Sidebar/Sidebar"
+import Loginpage from './Components/login/Loginpage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div  className='w-full h-full p-0 m-0'>
-      <Sidebar/>
+      <Sidebar/> 
+      <Loginpage/>
     </div>
   )
 }
