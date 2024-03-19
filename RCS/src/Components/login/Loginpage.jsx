90a577d553971c843fcaac8e787ea9296f6b9dd6
ROBIS <robis.rosbot@gmail.com>
2024-03-19 17:04:25 +0530

    first

--- RCS/src/Components/login/Loginpage.jsx
+++ RCS/src/Components/login/Loginpage.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Loginpage.css'
 const Loginpage = () => {
+  const [empType,setEmpType]=useState("employee")
   return (
     <div>
       <div className='login'>
     <h1>Login</h1>
+    <p>Login As</p>
+    <input type='radio'></input>
     <p>Username</p>
     <input className='username'></input>
     <p>Password</p>
