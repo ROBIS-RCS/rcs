721d2c3fd4844a8ea89d112992791e1ea8a56f0f
ROBIS <robis.rosbot@gmail.com>
2024-03-19 17:25:59 +0530

    final changes

--- RCS/src/Components/login/Loginpage.jsx
+++ RCS/src/Components/login/Loginpage.jsx
@@ -1,20 +1,33 @@
-import React,{useState} from 'react'
-import './Loginpage.css'
+import React, { useState } from 'react';
+import './Loginpage.css';
+
 const Loginpage = () => {
-  const [empType,setEmpType]=useState("employee")
+  const [empType, setEmpType] = useState("employee");
+
+  const handleEmpTypeChange = (event) => {
+    const newEmpType = event.target.value;
+    setEmpType(newEmpType);
+    console.log(newEmpType); // Log the updated state
+  };
+
   return (
     <div>
       <div className='login'>
-    <h1>Login</h1>
-    <p>Login As</p>
-    <input type='radio'></input>
-    <p>Username</p>
-    <input className='username'></input>
-    <p>Password</p>
-    <input className='password'></input><br/>  
-    <button className='Submit'>Submit</button>
+        <h1>Login</h1>
+        <p>Login As</p>
+        <select value={empType} onChange={handleEmpTypeChange}>
+          <option value="employee">Employee</option>
+          <option value="manager">Manager</option>
+          <option value="admin">Admin</option>
+        </select>
+        <p>Username</p>
+        <input className='username'></input>
+        <p>Password</p>
+        <input className='password'></input><br/>  
+        <button className='Submit'>Submit</button>
       </div>
     </div>
-  )
-}
-export default Loginpage
+  );
+};
+
+export default Loginpage;
