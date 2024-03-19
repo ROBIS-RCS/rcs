cc295fd5527b04bd9600826e1dc4e277a7583111
ROBIS <robis.rosbot@gmail.com>
2024-03-19 16:18:29 +0530

    final

--- RCS/src/Components/login/Loginpage.jsx
+++ RCS/src/Components/login/Loginpage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import './Loginpage.css'
 const Loginpage = () => {
   return (
     <div>
       <div className='login'>
     <h1>Login</h1>
+    <p>Username</p>
+    <input className='username'></input>
+    <p>Password</p>
+    <input className='password'></input><br/>  
+    <button className='Submit'>Submit</button>
       </div>
     </div>
   )
 }
-
 export default Loginpage
