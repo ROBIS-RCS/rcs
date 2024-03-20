b863dbd581ebd00831e3ce7d09e25b056ee4d404
Robis-RCS <robis.rcs@gmail.com>
2024-03-20 09:05:24 +0530

    20-March-First-Commit
    Exmaple Commit to check

--- RCS/src/Components/Sidebar/DropDown.jsx
+++ RCS/src/Components/Sidebar/DropDown.jsx
@@ -12,10 +12,10 @@ const DropDown = (props) => {
     <div className="w-full h-full flex flex-col items-center justify-center">
         <div className='w-[calc(100%-30px)] px-2 py-3 flex justify-between items-center bg-black rounded-[5px]' onClick={toggleArrow}>
             <span className="flex items-center justify-center gap-5">
-                <img src={System} alt="" className="w-[22%]" />
-               <p className="text-white font-semibold text-[20px]">{props.title}</p> 
+                <img src={System} alt="" className="w-[20%]" />
+               <p className="text-white font-semibold text-[18px]">{props.title}</p> 
             </span>
-            <img src={DropArrow} alt="" className={!clicked ?"duration-500 rotate-0" : "rotate-180 duration-500"} />
+            <img src={DropArrow} alt="" className={!clicked ?"duration-500 rotate-0 w-7" : "rotate-180 duration-500 w-7"} />
         </div>
         <div className="w-[calc(100%-30px)]  h-20 bg-red-100">
 
