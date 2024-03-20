baee6fb647665e8dd0b8577a5cace773fd8b1ab3
ROBIS <robis.rosbot@gmail.com>
2024-03-20 15:46:31 +0530

    new changes

--- RCS/src/Components/login/SignupPage.jsx
+++ RCS/src/Components/login/SignupPage.jsx
@@ -31,7 +31,7 @@ const SignupPage = () => {
   return (
     <div className='body'>
       <div className='signup'>
-        <h1>Sign up</h1>
+        <h1 className='signuphead'>Sign up</h1>
         <p>Enroll As</p>
         <select className='enroll-as' value={enrollAs} onChange={handleEnrollAsChange}>
           <option value="employee">Employee</option>
@@ -41,19 +41,19 @@ const SignupPage = () => {
         <div className="input-wrapper">
           <label className={username ? 'float-label' : ''}>Username</label>
           <input className='signupusername' value={username} onChange={handleUsernameChange}></input>
-        </div>
+        </div><br/>
         <div className="input-wrapper">
           <label className={email ? 'float-label' : ''}>Email ID</label>
           <input className='emailid' value={email} onChange={handleEmailChange}></input>
-        </div>
+        </div><br/>
         <div className="input-wrapper">
           <label className={password ? 'float-label' : ''}>Password</label>
           <input type="password" className='signuppassword' value={password} onChange={handlePasswordChange}></input>
-        </div>
+        </div><br/>
         <div className="input-wrapper">
           <label className={confirmPassword ? 'float-label' : ''}>Confirm Password</label>
           <input type="password" className='confirm-password' value={confirmPassword} onChange={handleConfirmPasswordChange}></input>
-        </div>
+        </div><br/>
         <p className='Existing-User'>Existing User? <a href="/">Login</a></p>
         <button className='signupSubmit'>Submit</button>
       </div>
