64abec78839b5281cd13ae0fe1d0d7f3634ab93a
Prasath <vengadeshprasathk07@gmail.com>
2024-03-20 14:28:27 +0530

    s

--- RCS/src/Components/login/SignupPage.jsx
+++ RCS/src/Components/login/SignupPage.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './SignupPage.css';
+import { supabase} from '../../../server/createClient';
+
 
 const SignupPage = () => {
+
+  
   return (
     <div className='body'>
       <div className='signup'>
