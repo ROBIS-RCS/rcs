b43aa66ec06ba0596a2946b9d3422dbf685f5c98
ROBIS <robis.rosbot@gmail.com>
2024-03-21 10:24:37 +0530

    yet

--- RCS/src/App.jsx
+++ RCS/src/App.jsx
@@ -5,7 +5,6 @@ import Sidebar from './Components/Sidebar/Sidebar';
 import Loginpage from './Components/login/Loginpage';
 import SignupPage from './Components/login/SignupPage';
 import { supabase } from '../server/createClient';
-
 function App() {
   const [count, setCount] = useState(0);
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route path='/' element={<Loginpage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/home' element={<Sidebar/>}></Route>
-          <Route path="/newsign" element={<Login/>}></Route>
         </Routes>
       </Router>
     </div>
