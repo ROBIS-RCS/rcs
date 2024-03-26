a8a6d2490e0a698c4ee8059aaa54f6dcd3063b8e
ROBIS <robis.rosbot@gmail.com>
2024-03-26 14:13:28 +0530

    new

--- RCS/src/Pages/Sub-Pages/Welcome.jsx
+++ RCS/src/Pages/Sub-Pages/Welcome.jsx
@@ -1,14 +1,16 @@
 import './Welcome.css'
 import RCSImage from "../../../src/assets/images/RCS.png";
-import logo1 from "../../../src/assets/Logo/Motherson.png"
+import logo1 from "../../../src/assets/Logo/Motherson.png";
+import Typewriter from 'typewriter-effect/dist/core';
 const Welcome = () => {
   return (
     <div >       
-          <img src={RCSImage} alt="RCS AGV"  className='RCSimg'/>  
+            
           <div className='wel'> 
           <img src={logo1} alt="logo1"  className='logo1'/>  <br/> 
           <p className='font'> Welcome to robis RCS</p>
           </div>              
+          <img src={RCSImage} alt="RCS AGV"  className='RCSimg'/>
     </div>
   )
 }
