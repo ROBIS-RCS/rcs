2b66bc52c156b4bf3c83e7345c91aca88ffc156b
Robis-RCS <robis.rcs@gmail.com>
2024-03-26 16:10:58 +0530

    new

--- RCS/src/Pages/Sub-Pages/Welcome.jsx
+++ RCS/src/Pages/Sub-Pages/Welcome.jsx
@@ -1,9 +1,22 @@
+import Typewriter from 'typewriter-effect';
 
+import welcomeMotherson from "../../assets/images/RCS.png"
 
 const Welcome = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center ">
-        <p>Welcome to motherson</p>
+    <div className="w-full h-full flex p-10 relative">
+        <p className='text-[60px] h-auto flex w-[380px]  origin-top font-bold hover:text-[#911515]'>
+        <Typewriter
+            options={{
+              strings: ["   Welcome to robis-RCS...    "],
+              autoStart: true,
+              loop: true,
+            }}
+        />
+        </p>
+        <img src={welcomeMotherson} alt="" className='absolute w-[550px] bottom-[-10px] right-[-10px]' />
+
+
     </div>
   )
 }
