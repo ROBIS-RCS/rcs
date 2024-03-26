a06e0b43dd774a56875100c6ea8da137278a3a62
ROBIS <robis.rosbot@gmail.com>
2024-03-26 11:57:16 +0530

    change

--- RCS/src/Pages/Sub-Pages/Welcome.jsx
+++ RCS/src/Pages/Sub-Pages/Welcome.jsx
@@ -1,9 +1,16 @@
+import './Welcome.css'
+import RCSImage from "../../../src/assets/images/RCS.png";
+import logo1 from "../../../src/assets/Logo/redlogo.png"
 const Welcome = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center ">
-        <p>Welcome to motherson</p>
+    <div className='container'>
+        <div className='hr'> 
+          <img src={RCSImage} alt="RCS AGV"  className='RCSimg'/>
+          <img src={logo1} alt="logo1"  className='logo1'/>          
+          <p className="heading">Welcome to robis RCS </p>
+       
+        </div>     
     </div>
   )
 }
-
 export default Welcome
