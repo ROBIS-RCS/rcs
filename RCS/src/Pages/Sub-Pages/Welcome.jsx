356d272f7711e6d9993911c20e69f9ca0cbe7c00
ROBIS <robis.rosbot@gmail.com>
2024-03-28 12:13:22 +0530

    new

--- RCS/src/Pages/Sub-Pages/Welcome.jsx
+++ RCS/src/Pages/Sub-Pages/Welcome.jsx
@@ -3,7 +3,7 @@ import Mlogo from '../../assets/Logo/redlogo.png'
 import welcomeMotherson from "../../assets/images/RCS.png"
 import Particle from '../../Components/ParticleJS/Particle';
 import Slideshow from '../../Components/Slideshow/Slideshow.jsx';
-import GIF from "../../assets/GIF/www.gif"
+import GIF from "../../assets/GIF/Designer.png"
 
 const Welcome = () => {
   return (
@@ -14,7 +14,7 @@ const Welcome = () => {
       <div className='w-full h-full'>
          <div className="w-full h-full flex p-10 relative"> 
         
-          <p className='text-[62px] h-auto flex w-[400px]  origin-top font-bold hover:text-[#911515] drop-shadow-lg'>
+          <p className='text-[70px] h-auto flex w-[450px]  origin-top font-bold hover:text-[#911515] drop-shadow-lg'>
           
           <Typewriter
               options={{
@@ -23,9 +23,10 @@ const Welcome = () => {
                 loop: true,
               }}
           />
+          
           </p>          
           <img src={Mlogo} alt="" className='absolute w-[50px] top-[10px] right-[10px] drop-shadow-lg' /> 
-                   
+          <img src={GIF} alt="" className='absolute w-[850px] bottom-[20px] left-[-50px] drop-shadow-2xl' />          
           <Slideshow />
       </div>
       </div>     
