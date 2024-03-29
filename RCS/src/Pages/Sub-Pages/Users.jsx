c711daa0fab7a37e192a6404e354b9c55fe761a3
Robis-RCS <robis.rcs@gmail.com>
2024-03-29 13:02:10 +0530

    Changed the Welcome page

--- RCS/src/Pages/Sub-Pages/Users.jsx
+++ RCS/src/Pages/Sub-Pages/Users.jsx
@@ -1,62 +1,49 @@
-import panda from "../../assets/Profile/Sudhagaran.jpg"
-import { MdDeleteForever } from "react-icons/md"; //Delete Icon
-import { FaCircleInfo } from "react-icons/fa6";  //Info Icon
-import Custom_img from "../../Components/Custom_Image/Custom_img";
-
-const dataProf = [
-    {
-        name: "Username",
-        des: "User Designation",
-        prof: "../../assets/Profile/Sudhagaran.jpg"
-    },
-    {
-        name: "Username",
-        des: "User Designation",
-        prof: "../../assets/Profile/Sudhagaran.jpg"
-    },
-    {
-        name: "Username",
-        des: "User Designation",
-        prof: "../../assets/Profile/Sudhagaran.jpg"
-    },
-    {
-        name: "Username",
-        des: "User Designation",
-        prof: "../../assets/Profile/Sudhagaran.jpg"
-    },
-]
-
-
+import React, { useState, useEffect } from 'react';
+import { MdDeleteForever } from "react-icons/md";
+import { FaCircleInfo } from "react-icons/fa6";
+import AddMore from "../../assets/Icons/Add More Icon.svg";
 
 const Users = () => {
-  return (
-    <div className="w-full p-4 flex flex-wrap align-top gap-4 rounded-[15px]">
-        {
-            dataProf.map((cont, key)=> {
-                return(
-                    <div key={key} className="w-[400px] h-[200px] rounded-[10px] flex flex-col p-5 items-center bg-gradient-to-tl from-gray-300  to-red-100 justify-between duration-300 hover:shadow-xl">
-                        <div className="w-full items-center flex gap-4">
-                            <Custom_img size={64} imageURL={cont.prof}/>
-                            <span>
-                                <p className="text-[22px] hover:text-[#f00] duration-200 text-[#0a0000] cursor-default font-bold">{cont.name}</p>
-                                <p className="text-[16px] font-semibold hover:drop-shadow-xl text-[#999999] cursor-default">{cont.des}</p>
-                            </span>
-                        </div>
-                        <div className="w-full  gap-3 grid grid-cols-[2fr,2fr,1fr]">
-                            <button className="flex gap-1 w-full py-2 text-white font-semibold rounded-[5px] bg-red-700 hover:bg-red-600 active:scale-[0.97] items-center justify-center"><MdDeleteForever size={21} color="white" />Delete</button>
-                            <button className="flex w-full py-2 text-white font-semibold rounded-[5px] bg-black items-center justify-center active:scale-[0.97]">ABCD</button>
-                            <button className="flex w-full py-2 font-semibold rounded-[5px] bg-black items-center justify-center active:scale-[0.97]"><FaCircleInfo size={20} color="white"/></button>
-                        </div>
+    const [profiles, setProfiles] = useState([]);
+
+    // useEffect to handle side effects related to state changes
+    useEffect(() => {
+
+        localStorage.setItem('profiles', JSON.stringify(profiles));
+    }, [profiles]); // dependencies array: run effect whenever 'profiles' state changes
+
+    const addProf = () => {
+        const profData = {
+            name: "Username",
+            des: "User Designation",
+            prof: "../../assets/Profile/Sudhagaran.jpg"
+        };
+        setProfiles(prevProfiles => [...prevProfiles, profData]);
+    };
+
+    return (
+        <div className="w-full p-4 flex flex-wrap align-top gap-4 rounded-[15px]">
+            { profiles.map((cont, index) => (
+                <div key={index} className="w-[400px] h-[180px] rounded-[10px] flex flex-col p-5 items-center bg-gradient-to-tl from-gray-300  to-red-100 justify-between duration-300 hover:shadow-xl">
+                    <div className="w-full items-center flex gap-4">
+                        <img src={cont.prof} alt="" />
+                        <span>
+                            <p className="text-[22px] hover:text-[#f00] duration-200 text-[#0a0000] cursor-default font-bold">{cont.name}</p>
+                            <p className="text-[16px] font-semibold hover:drop-shadow-xl text-[#999999] cursor-default">{cont.des}</p>
+                        </span>
                     </div>
-                )
-            })
-        }
-        
-        <div className="w-[400px] h-[200px] rounded-[10px] hover:shadow-xl duration-200 border-2">
-            
+                    <div className="w-full  gap-3 grid grid-cols-[2fr,2fr,1fr]">
+                        <button className="flex gap-1 w-full py-2 text-white font-semibold rounded-[5px] bg-red-700 hover:bg-red-600 active:scale-[0.97] items-center justify-center"><MdDeleteForever size={21} color="white" />Delete</button>
+                        <button className="flex w-full py-2 text-white font-semibold rounded-[5px] bg-black items-center justify-center active:scale-[0.97]">ABCD</button>
+                        <button className="flex w-full py-2 font-semibold rounded-[5px] bg-black items-center justify-center active:scale-[0.97]"><FaCircleInfo size={20} color="white"/></button>
+                    </div>
+                </div>
+            ))}
+            <div className="w-[400px] h-[200px] rounded-[10px] flex items-center justify-center hover:shadow-xl duration-200 border-2">
+                <img src={AddMore} alt="" className="w-[25%] opacity-30 hover:opacity-70 active:scale-95 duration-200" onClick={addProf} />
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Users
+export default Users;
--- RCS/src/Pages/Sub-Pages/Welcome.jsx
+++ RCS/src/Pages/Sub-Pages/Welcome.jsx
@@ -4,6 +4,56 @@ import welcomeMotherson from "../../assets/images/RCS.png"
 import Particle from '../../Components/ParticleJS/Particle';
 import Slideshow from '../../Components/Slideshow/Slideshow.jsx';
 import GIF from "../../assets/GIF/Designer.png"
+import Marquee from "react-fast-marquee";
+
+//images for Marquee
+import image1 from "../../assets/images/Marquee/ASRS.png"
+import image2 from "../../assets/images/Marquee/BTP.png"
+import image3 from "../../assets/images/Marquee/CB.png"
+import image4 from "../../assets/images/Marquee/Compactagv.png"
+import image5 from "../../assets/images/Marquee/counteramr.png"
+import image6 from "../../assets/images/Marquee/forkover.png"
+import image7 from "../../assets/images/Marquee/Pallet.png"
+import image8 from "../../assets/images/Marquee/RTP.png"
+
+const MarqueeImages = [
+  {
+    text: "ASRS Bin Shuttle",
+    img: image1
+  },
+  {
+    text: "Pallet Truck AMR",
+    img: image2
+  },
+  {
+    text: "Counterbalance Stacker AMR",
+    img: image3
+  },
+  {
+    text: "Compact 360 AGV",
+    img: image4
+  },
+  
+  {
+    text: "Fork over AMR",
+    img: image6
+  },
+  {
+    text: "Compact 360 AMR ",
+    img: image5
+  },
+  {
+    text: "Bin To Person AGV / AMR",
+    img: image7
+  },
+  {
+    text: "Rack To Person AGV",
+    img: image8
+  },
+]
+
+
+
 
 const Welcome = () => {
   return (
@@ -12,22 +62,40 @@ const Welcome = () => {
           <Particle/>
         </div>
       <div className='w-full h-full'>
-         <div className="w-full h-full flex p-10 relative"> 
+         <div className="w-full h-full flex flex-col justify-between p-10 relative"> 
         
-          <p className='text-[70px] h-auto flex w-[450px]  origin-top font-bold hover:text-[#911515] drop-shadow-lg'>
+          <p className='text-[340%] h-auto flex w-[450px]  origin-top font-bold hover:text-[#911515] drop-shadow-lg'>
+          
           
           <Typewriter
-              options={{
-                strings: ["   Welcome to robis-RCS...    "],
-                autoStart: true,
-                loop: true,
-              }}
+            onInit={(typewriter) => {
+              typewriter.typeString('  Welcome to robis-RCS...  ')
+          .pauseFor(2500)
+          .start();
+            }}
           />
-          
           </p>          
           <img src={Mlogo} alt="" className='absolute w-[50px] top-[10px] right-[10px] drop-shadow-lg' /> 
-          <img src={GIF} alt="" className='absolute w-[850px] bottom-[20px] left-[-50px] drop-shadow-2xl' />          
-          <Slideshow />
+          <div className='w-full h-[300px] flex justify-between items-center'>
+            <div className='w-[50%] h-full flex justify-end items-end relative'>
+              <img src={GIF} alt="Storage" className='object-contain absolute left-[-60px] top-10 hover:drop-shadow-xl' />
+            </div>
+            <div className='w-[60%] h-[340px] flex flex-col items-center justify-center p-2'>
+              <Marquee pauseOnHover={false} speed={50}  >
+                {
+                    MarqueeImages.map((marquee, key)=> {
+                      return(
+                        <div className='w-[320px] h-[320px] mx-2 flex items-center justify-center p-2 rounded-20px hover:shadow-xl hover:text-[#911515] duration-200 relative object-contain rounded-2xl overflow-hidden'>
+                          <img src={marquee.img} alt="" className='w-[290px] h-[290px] object-contain overflow-hidden rounded-2xl hover:drop-shadow-xl'/>
+                          <p className='absolute bottom-4 left-5 font-semibold px-4 py-1 hover:bg-white bg-opacity-55 rounded-full'>{marquee.text}</p>
+                        </div>
+                      )
+                    })
+                }
+              </Marquee>
+            </div>
+          </div>
+
       </div>
       </div>     
     </div>
