fe05e3b77bb06f9ec66da79d3d76efe66f7fcfba
Robis-RCS <robis.rcs@gmail.com>
2024-04-10 09:30:51 +0530

    Auto reload for the other services are done

--- RCS/src/Components/Navbar/Sidebar.jsx
+++ RCS/src/Components/Navbar/Sidebar.jsx
@@ -7,41 +7,43 @@ import { TiHomeOutline } from "react-icons/ti"; //Home
 
 const Sidebar = () => {
   return (
-    <motion.div
-    className="absolute top-0 left-0 h-screen bg-[#911515] px-3 pt-32 flex flex-col gap-10 "
-    initial={{
-        width: "5rem",
-    }}
-    whileHover={{width: "16rem"}}
-    end={{width: "5rem"}}
-    transition={{
-        duration: 0.5,
-        type:"spring",
-        damping: 10,
-        stiffness: 130
-    }}
-    >
-        <div className="flex flex-col gap-3">
+    <div className='absolute top-0 left-0 h-screen'>
+      <motion.div
+      className="h-screen bg-[#911515] px-3 pt-32 flex flex-col gap-10 "
+      initial={{
+          width: "5rem",
+      }}
+      whileHover={{width: "16rem"}}
+      end={{width: "5rem"}}
+      transition={{
+          duration: 0.5,
+          type:"spring",
+          damping: 10,
+          stiffness: 130
+      }}
+      >
+          <div className="flex flex-col gap-3">
 
-          <SidebarLinks name={"Home"} className="min-w-8 w-8">
+            <SidebarLinks name={"Home"} className="min-w-8 w-8">
+              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[1.5] stroke-white min-w-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              </svg>
+            </SidebarLinks>
+
+            <SidebarLinks name={"Notification"} className="min-w-8 w-8">
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[1.5] stroke-white min-w-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-            </svg>
-          </SidebarLinks>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+              </svg>
+            </SidebarLinks>
 
-          <SidebarLinks name={"Notification"} className="min-w-8 w-8">
-          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[1.5] stroke-white min-w-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-          </SidebarLinks>
-
-          <SidebarLinks name={"User"} className="min-w-8 w-8">
-                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[1.5] stroke-white min-w-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-          </SidebarLinks>
-        </div>
-    </motion.div>
+            <SidebarLinks name={"User"} className="min-w-8 w-8">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[1.5] stroke-white min-w-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+            </SidebarLinks>
+          </div>
+      </motion.div>
+    </div>
   )
 }
 
--- RCS/src/Components/Navbar/TopNav.jsx
+++ RCS/src/Components/Navbar/TopNav.jsx
@@ -1,29 +1,43 @@
 import {motion} from "framer-motion"
 import MothersonLogo from "../../assets/Logo/MOTHERSON_white.png"
 import { MdOutlineLogout } from "react-icons/md";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useSignOut from 'react-auth-kit/hooks/useSignOut'
+import useAuthUser from 'react-auth-kit/hooks/useAuthUser'
 
 
 const TopNav = (props) => {
-    const logout = useSignOut()
+    const auth = useAuthUser()
+    const [logout, setLogout] = useState(false)
     const [viewProfile, setViewProfile] = useState(false);
     const Profile = () =>{
         setViewProfile(!viewProfile)
     }
+    const signout =  useSignOut();
+
+    useEffect(()=> {
+        
+        if(logout) {
+            signout()
+            window.location.reload()
+        }
+    },[logout])
 
   return (
     <div className="fixed top-0 left-0 right-0 gap-5 bg-[#911515] w-full h-20 flex items-center justify-between pl-5 pr-10">
         <img src={MothersonLogo} alt="" className="h-9" />
         <div className="flex items-center justify-center gap-7">
         <img src="" alt="" className="w-12 h-12 rounded-full bg-[#fff] mb-2" onClick={Profile}/>
-        <div className={viewProfile ? "w-[15rem] h-fit p-5 bg-black shadow-xl drop-shadow-md absolute bottom-[-210px] right-1 rounded-[15px]" : "hidden"}>
-            <span>
-                <img src="" alt="" className="w-12 h-12 rounded-full bg-[#fff] mb-2"/>
-                <p className="text-[20px] font-bold text-white font-mono tracking-wide">Username</p>
-                <p className="text-[14px] font-semibold text-red-500">User designation</p>
+        <div className={viewProfile ? "w-[15rem] h-fit p-5 bg-black shadow-xl drop-shadow-md absolute bottom-[-160px] right-1 rounded-[15px]" : "hidden"}>
+            <span className="flex gap-4 items-center">
+                <img src="" alt="" className="w-12 h-12 rounded-full bg-[#fff]"/>
+                <span >
+                    <p className="text-[20px]  font-bold text-white font-mono tracking-wide">{auth.user}</p>
+                    <p className="text-[14px] font-semibold text-red-500">{auth.role}</p>
+                </span>
+                
             </span>
-            <button className="w-full rounded-full flex items-center justify-center gap-3 bg-red-800 h-10 mt-5 text-white font-bold " onclick={() => logout()}>
+            <button className="w-full rounded-full flex items-center justify-center gap-3 bg-red-800 h-10 mt-5 text-white font-bold " onClick={() => setLogout(!logout)}>
                 logout 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
